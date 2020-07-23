1176972b97c5c7c495f9530031b9655cec12d9fa
abhi78945 <52705278+abhi78945@users.noreply.github.com>
2020-07-23 20:18:07 +0530

    assignment 4, question 5

---
+++ salesmancom.js
@@ -0,0 +1,92 @@
+/*
+
+    if (sales < 5000){
+
+        var percent = sales*0.2; 
+
+    }
+
+    else if (sales >= 5001 && sales < 10000) {
+
+        var percent = sales*0.5;
+
+    }
+
+    else if (sales >= 10001 && sales < 20000){
+
+        var percent = sales*0.7;
+
+    }
+
+    else (sales > 20000)  
+{
+        var percent = sales*1;
+
+} */
+
+
+
+//var salary = 0;
+
+        //var grossSales = 0;
+
+        //var commission = 0;
+//var rangeOfsales=4 ;
+     
+      var salary = prompt();
+          //var str= salary
+var a=[];
+//for (var i = 0; i < rangeOfsales.length; ++i)
+         //rangeOfsales[ i ] = 4;
+
+
+
+
+
+           if(salary >= 0 && salary <= 5000)
+
+             //   rangeOfsales[0] += 1;
+             {
+                console.log  (a[0]=salary*0.2 ) ;
+                }
+
+            if(salary >= 5001 && salary <= 10000)
+
+               // rangeOfsales[1] += 1;
+            {
+                console.log  ( a[1]=salary*0.5  ) ;
+            }
+
+            if(salary >= 10001 && salary <= 20000)
+
+               // rangeOfsales[2] += 1;
+               {
+                console.log  (a[2]=salary*0.7  ) ;
+               }
+
+            if( salary > 20000)
+
+               // rangeOfsales[3] += 1;
+               {
+                console.log  (a[3]=salary*1  ) ;
+               }
+
+var add = a[0] + a[1] + a[2] +a[3];
+
+    console.log(add);
+
+               
+                
+                           
+                                 /* console.log  (rangeOfsales[0]  ) ;
+                                  console.log  (rangeOfsales[1]  ) ;
+                                  console.log  (rangeOfsales[2]  ) ;
+                                  console.log  (rangeOfsales[3]  ) ;*/
+                
+                
+
+                        
+            
+
+                        
+                
